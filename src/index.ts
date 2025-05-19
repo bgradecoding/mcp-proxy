21f889b67bc9824c828cf8162793e983efcd6aa2
Jung Jinho <83222882+bgradecoding@users.noreply.github.com>
2025-05-19 16:09:33 +0900

    docs: clarify CLI stdio limitations

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export { proxyServer } from "./proxyServer.js";
 export { startHTTPStreamServer } from "./startHTTPStreamServer.js";
 export { startSSEServer } from "./startSSEServer.js";
 export { ServerType, startStdioServer } from "./startStdioServer.js";
+export type { StartStdioServerOptions } from "./startMultiStdioServer.js";
+export { startMultiStdioServer } from "./startMultiStdioServer.js";
 export { tapTransport } from "./tapTransport.js";
 export { createHeaderAuth, headerAuth } from "./headerAuth.js";
 export { createAuthenticatedSSEClientTransport } from "./authenticatedSSEClientTransport.js";
